b04afe6939482dbe6fc3767b5adcc737b6ded773
sidney-souza <83144292+sidney-souza@users.noreply.github.com>
2021-08-30 14:29:06 -0300

    Revert "🔙🔚Finalizando Projeto 📌"

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,13 @@
-import express, { Express } from 'express'
+import express from 'express'
 import cors from 'cors'
-import { AddressInfo } from 'net';
 
-const app: Express = express()
+const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-const server = app.listen(process.env.PORT || 3003, () => {
-    if (server) {
-        const address = server.address() as AddressInfo;
-        console.log(`Server is running in http://localhost:${address.port}`);
-    } else {
-        console.error(`Failure upon starting server`);
-    };
-});
+app.listen(3003, ()=>{
+    console.log('Servidor rodando na porta 3003')
+})
 
-export default app;
+export default app
--- src/types.ts
+++ src/types.ts
@@ -1,62 +1,14 @@
-export type date = {
-   day: number,
-   month: number,
-   year: number
+export type user = {
+   id: string
+   name: string
+   nickname: string
+   email: string
+   address: string
 }
 
-export type student = {
-   id: number,
-   name: string,
-   email: string,
-   birthDate: string
-};
-
-export type studentBirth = {
-   birthDate: string
+export type addressInfo = {
+   street:string
+   neighborhood:string
+   city:string
+   state:string
 }
-
-export type teacher = {
-   id: number,
-   name: string,
-   email: string,
-   birthDate: string
-};
-
-export type classes = {
-   id: number,
-   name: string,
-   startDate: string,
-   endDate: string,
-   module: string,
-   period?: string
-}
-
-export type hobby = {
-   id: number,
-   description: string
-};
-
-export type specialty = {
-   id: number,
-   description: string
-};
-
-export type studHobDependencies = {
-   student_id: number,
-   hobbies_id: number
-};
-
-export type teachSpecDependencies = {
-   teacher_id: number,
-   specialties_id: number
-};
-
-export type classStudDependencies = {
-   class_id: number,
-   student_id: number
-};
-
-export type classTeachDependencies = {
-   class_id: number,
-   teacher_id: number
-};
